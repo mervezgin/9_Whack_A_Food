4aa867cffcabc85ed99acc409cdde1c6206db4d0
mervezgin <mervezgin@gmail.com>
2024-04-22 17:04:57 +0300

    webgl sharing

--- Assets/Challenge 5/Scripts/DestroyObjectX.cs
+++ Assets/Challenge 5/Scripts/DestroyObjectX.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 
 public class DestroyObjectX : MonoBehaviour
 {
-    /*
     void Start()
     {
         Destroy(gameObject, 2); // destroy particle after 2 seconds
     }
-    */
 
 }
